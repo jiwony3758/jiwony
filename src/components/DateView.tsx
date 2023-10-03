b0182bfc8d2c72856508798ae947c4a570dad084
jiwony3758 <jiwony3758@gmail.com>
2023-10-03 17:22:32 +0900

    Design: Post DateView 월, 일 두 자리 고정

--- src/components/DateView.tsx
+++ src/components/DateView.tsx
@@ -1,8 +1,8 @@
 export default function DateView({ dateString }: { dateString: string }) {
   const date = new Date(dateString);
   const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
   return (
     <time className="post-date" dateTime={dateString}>
       {year}-{month}-{day}
