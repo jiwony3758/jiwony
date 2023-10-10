61e1b9f05c1a40528440b9d39bb30c0d3b38e445
jiwony3758 <jiwony3758@gmail.com>
2023-10-11 03:05:58 +0900

    Chore: script 에러 핸들러 try catch 추가

--- scripts/create-md.ts
+++ scripts/create-md.ts
@@ -5,7 +5,8 @@ import { exit } from "process";
 const fileName = process.argv[3];
 const category = process.argv[2] ? process.argv[2] : "";
 
-const fullPath = path.join(process.cwd(), `/src/content/posts/${category}/`);
+const directoryPath = path.join(process.cwd(), `/src/content/posts/${category}/`);
+const filePath = path.join(directoryPath, fileName);
 
 const content = `---
 title: ""
@@ -17,20 +18,24 @@ tags: ""
 `
 
 if(category !== "") {
-  const isExistDirectory = fs.existsSync(fullPath);
-  if(!isExistDirectory) fs.mkdirSync(fullPath);
+  const isExistDirectory = fs.existsSync(directoryPath);
+  if(!isExistDirectory) fs.mkdirSync(directoryPath);
 }
 
-const filePath = path.join(process.cwd(), `/src/content/${category}/${fileName}`);
+try{
+  const isExistFile = fs.existsSync(filePath);
+  if(isExistFile) throw new Error("Already exist file...");
+  else{
+    fs.writeFileSync(filePath, content);
 
-if(fs.readFileSync(filePath)) {
+    console.info(`Success make Contents!!`);
+    console.info(`Directory: ${category}`);
+    console.info(`File: ${fileName}`);
+  }
+}catch(e){
+  console.log(filePath);
   console.info(`Failed make Contents!!`);
-  console.info(`Already exist file...`);
+  console.info(e);
   exit();
 }
 
-fs.writeFileSync(filePath, content);
-
-console.info(`Success make Contents!!`);
-console.info(`Directory: ${category}`);
-console.info(`File: ${fileName}`);
