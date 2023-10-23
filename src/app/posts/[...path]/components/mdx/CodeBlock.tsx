9bac7296b8b5bdbac8be6341e8ecc130c0e34bab
jiwony3758 <jiwony3758@gmail.com>
2023-10-23 16:19:02 +0900

    Feat: mdx 컴포넌트 추가

---
+++ src/app/posts/[...path]/components/mdx/CodeBlock.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { shadesOfPurple } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+// const CopyButton = ({ target }: { target: string }) => {
+//   const handleCopy =  async () => {
+//     try{
+//       if (target) {
+//         await navigator.clipboard.writeText(target);
+//         alert("copy")
+//       }
+//     }catch(e){
+//       alert(`copy failed ${e}`)
+//     }
+//   }
+//   return <button 
+//     style={{
+//       position: "absolute",
+//     }}
+//     onClick={handleCopy}
+//     >copy
+//     </button>
+// }
+
+
+
+export default function CodeBlock({
+  title, 
+  language,
+  children,
+}: { 
+  title: string, 
+  language: string,
+  children: string 
+}) {
+  return (
+    <div className="relative">
+      {/* <CopyButton target={children}/> */}
+      { title && <div className="code-title">{title}</div> }
+      <SyntaxHighlighter 
+        language={language} 
+        style={shadesOfPurple}
+        showLineNumbers 
+      >
+        {children}
+      </SyntaxHighlighter>
+    </div>
+  )
+} 
--- src/app/posts/[...path]/page.tsx
+++ src/app/posts/[...path]/page.tsx
@@ -3,6 +3,10 @@ import React from "react";
 import "./style.css";
 import di from "@/di";
 import { PostVM } from "@/vm/Post";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import CodeBlock from "./components/mdx/CodeBlock";
+
+const components = { CodeBlock }
 
 export const dynamicParams = false;
 
@@ -19,17 +23,21 @@ export default async function Post({ params }: { params: { path: string[] } }) {
   const { path } = params;
   const postData = await di.post.getPostDataByRoutingPath(path);
   const postVM = new PostVM(postData);
-
   return (
     <>
       <div className="post-header">
         <h1 className="title">{postVM.title as string}</h1>
         <DateView dateString={postVM.date as string} />
       </div>
-      <div
+      { postVM.content.mdxSource && <div className="content"><MDXRemote 
+        source={postVM.content.mdxSource} 
+        components={components}
+      /> </div>}
+      { postVM.content.htmlContent && <div
         className="content"
-        dangerouslySetInnerHTML={{ __html: postVM.content }}
-      />
+        dangerouslySetInnerHTML={{ __html: postVM.content.htmlContent }}
+      /> }
+      
     </>
   );
 }
