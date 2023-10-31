823350356c4cba83aa8c46bd3cb04c7ce48a763b
jiwony3758 <jiwony3758@gmail.com>
2023-10-31 17:58:06 +0900

    Chore: tailwind setting

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "background-rgb": "var(--background-rgb)",
+        "background-tag-rgb": "var(--background-tag-rgb)",
+        "background-tag-hover-rgb": "var(--background-tag-hover-rgb)",
+        "text-basic": "var(--text-basic)",
+        "text-primary": "var(--text-primary)",
+        "text-description": "var(--text-description)",
+        "text-description2": "var(--text-description2)",
+        "text-link": "var(--text-link)",
+        "border-primary": "var(--border-primary)"
+      }
+    },
+  },
+  plugins: [],
+}
+
