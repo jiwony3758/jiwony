b3cf4ec0a66f7ca689b9abc01343513c897267d7
jiwony3758 <jiwony3758@gmail.com>
2023-11-02 15:56:45 +0900

    Fix: fix repository, infrastructure di

--- src/di/infrastructures.ts
+++ src/di/infrastructures.ts
@@ -1,9 +1,12 @@
 import { FileHandler } from "@/adapters/infrastructures/FileHandler";
+import { NotionClient } from "@/adapters/infrastructures/NotionClient";
 
 export interface IInfrastructures {
   fileHandler: FileHandler;
+  notionClient: NotionClient;
 }
 
 export default (): IInfrastructures => ({
   fileHandler: new FileHandler(),
+  notionClient: new NotionClient({ auth: process.env.NOTION_SECRET }),
 });
--- src/di/repositories.ts
+++ src/di/repositories.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { IInfrastructures } from "./infrastructures";
 
 const postDirectory = path.join(process.cwd(), "src/contents/posts");
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 export interface IRepositories {
   postRepository: PostRepository;
@@ -10,7 +11,9 @@ export interface IRepositories {
 
 export default (infrastructures: IInfrastructures): IRepositories => ({
   postRepository: new PostRepository(
+    NOTION_DATABASE_ID as string,
     postDirectory,
-    infrastructures.fileHandler
+    infrastructures.fileHandler,
+    infrastructures.notionClient
   ),
 });
